955ddb90d38b73710587f056c62704a06220fd21
st0neshi <imericshi@gmail.com>
2021-09-15 04:43:21 -0400

    fixed auto locate

--- js/index.js
+++ js/index.js
@@ -18,13 +18,13 @@ window.onscroll = () => {
     section.forEach(sec => {
         let top = window.scrollY;
         let height = sec.offsetHeight;
-        let offset = sec.offsetTop - 150;
+        let offset = sec.offsetTop - 100;
         let id = sec.getAttribute('id');
 
         if(top >= offset && top < offset + height){
             navLinks.forEach(links => {
               links.classList.remove('active');
-              document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+              document.querySelector('.header .navi a[href*='+id+']').classList.add('active');
             });
           };
     });
