55f8c337c5481569e0af20faacd47c25db1ef0ca
st0neshi <imericshi@gmail.com>
2021-09-15 04:13:39 -0400

    finished menu about reviews contact tabs

--- js/index.js
+++ js/index.js
@@ -7,9 +7,27 @@ bars.onclick = () => {
     navi.classList.toggle('active');
 }
 
-window.onscroll = () =>{
+// scolling
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('.header .navi a');
+
+window.onscroll = () => {
     bars.classList.remove('fa-times');
     navi.classList.remove('active');
+
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let height = sec.offsetHeight;
+        let offset = sec.offsetTop - 150;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+              links.classList.remove('active');
+              document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+            });
+          };
+    });
 }
 
 // for pop up search form
@@ -20,3 +38,55 @@ document.querySelector('#search-button').onclick = () => {
 document.querySelector('#close-button').onclick = () => {
     document.querySelector('#search-content').classList.remove('active');
 }
+
+// gallery
+var swiper = new Swiper(".gallery", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 5500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    // navigation: {
+    //   nextEl: ".swiper-button-next",
+    //   prevEl: ".swiper-button-prev",
+    // },
+    loop: true,
+});
+
+// review
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+        delay: 5500,
+        disableOnInteraction: false,
+    },
+    // pagination: {
+    //     el: ".swiper-pagination",
+    //     clickable: true,
+    // },
+    // navigation: {
+    //   nextEl: ".swiper-button-next",
+    //   prevEl: ".swiper-button-prev",
+    // },
+    loop: true,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        640: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
