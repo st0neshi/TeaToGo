fd08b2951776d8a42adf2220d7ba06986aede8f8
st0neshi <imericshi@gmail.com>
2021-09-14 20:17:27 -0400

    finished pop up search form

--- js/index.js
+++ js/index.js
@@ -1,7 +1,22 @@
-let bars = document.querySelector('#bars-icon');
+// for navi bars
+let bars = document.querySelector('#bars-button');
 let navi = document.querySelector('.navi');
 
 bars.onclick = () => {
     bars.classList.toggle('fa-times');
     navi.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    bars.classList.remove('fa-times');
+    navi.classList.remove('active');
+}
+
+// for pop up search form
+document.querySelector('#search-button').onclick = () => {
+    document.querySelector('#search-content').classList.toggle('active');
+}
+
+document.querySelector('#close-button').onclick = () => {
+    document.querySelector('#search-content').classList.remove('active');
 }
